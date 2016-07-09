86161a1517b5b0cdec57d1a6e88439e0cdef3b03
Minor Gordon <minorg@server.fake>
2016-07-09 17:21:42 -0500

    don't need libthryft

--- thrift/devbin/generate_src.py
+++ thrift/devbin/generate_src.py
@@ -35,19 +35,12 @@ class Main(thryft.main.Main):
     def _clean(self):
         for dir_path in (
              os.path.join(ROOT_DIR_PATH, 'py', 'src', 'yomeka', 'api'),
-             os.path.join(ROOT_DIR_PATH, 'py', 'src', 'thryft'),
         ):
             if os.path.isdir(dir_path):
                 shutil.rmtree(dir_path)
 
     def _compile(self):
         self._clean()
-
-        # Copy thryft/lib/py to py/src
-        shutil.copytree(
-            os.path.join(THRYFT_ROOT_DIR_PATH, 'lib', 'py', 'src', 'thryft'),
-            os.path.join(ROOT_DIR_PATH, 'py', 'src', 'thryft')
-        )
 
         thrift_src_root_dir_path = os.path.join(ROOT_DIR_PATH, 'thrift', 'src')
 
